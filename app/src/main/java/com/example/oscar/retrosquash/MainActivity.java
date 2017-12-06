c82fd0418651a9544f500adf707abc1684c858c3
Oscar Morales <oscar4m2@gmail.com>
2017-12-06 10:38:22 -0500

    Adding files to Repo - first commit

---
+++ app/src/main/java/com/example/oscar/retrosquash/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.oscar.retrosquash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import java.io.IOException;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Canvas canvas;
+    SquashCourtView squashCourtView;
+
+    //Sound
+    //Init sound variables
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
+    int sample4 = -1;
+
+    //for getting display detail like the number of pixels
+    Display display;
+    Point size;
+    int screenWidth;
+    int screenHeight;
+
+    //Game objects
+    int racketWidth;
+    int racketHeight;
+    Point racketPosition;
+
+    Point ballPosition;
+    int ballWidth;
+
+    //for ball movement
+    boolean ballIsMovingLeft;
+    boolean ballIsMovingRight;
+    boolean ballIsMovingUp;
+    boolean ballIsMovingDown;
+
+    //for racket movement
+    boolean racketIsMovingLeft;
+    boolean racketIsMovingRight;
+
+    //stats
+    long lastFrameTime;
+    int fps;
+    int score;
+    int lives;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        squashCourtView = new SquashCourtView(this);
+        setContentView(squashCourtView);
+
+        //sound code
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+        try {
+            //Catch objects of the 2 requied classes
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+            //create our three fx in memory ready for use
+            descriptor = assetManager.openFd("Jump1.ogg");
+            sample1 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("Jump2.ogg");
+            sample2 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("Jump3.ogg");
+            sample3 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("Jump4.ogg");
+            sample4 = soundPool.load(descriptor, 0);
+        } catch (IOException e) {
+            //catch exceptions here
+        }
+
+        //COuld this be an object with getters and setters
+        //Dont want just anyone changing screen size
+        //Get the screen size in pixels
+        display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+
+        //The game objects
+        racketPosition = new Point();
+        racketPosition.x = screenWidth / 2;
+        racketPosition.y = screenHeight - 20;
+        racketWidth = screenWidth / 8;
+        racketHeight = 10;
+
+        ballWidth = screenWidth / 35;
+        ballPosition = new Point();
+        ballPosition.x = screenWidth / 2;
+        ballPosition.y = 1 + ballWidth;
+
+        lives = 3;
+
+    }
+
+    class SquashCourtView extends SurfaceView implements Runnable {
+        Thread ourThread = null;
+        SurfaceHolder ourHolder;
+        volatile boolean playingSquash;
+        Paint paint;
+
+        public SquashCourtView (Context context) {
+            super(context);
+            ourHolder = getHolder();
+            paint = new Paint();
+            ballIsMovingDown = true;
+
+            //Send the ball in random direction
+            Random randomNumber = new Random();
+            int ballDirection = randomNumber.nextInt(3);
+            switch (ballDirection) {
+                case 0:
+                    ballIsMovingLeft = true;
+                    ballIsMovingRight = false;
+                    break;
+
+                case 1:
+                    ballIsMovingRight = false;
+                    ballIsMovingLeft = true;
+                    break;
+
+                case 2:
+                    ballIsMovingLeft = false;
+                    ballIsMovingRight = false;
+                    break;
+            }
+
+        }
+
+        @Override
+        public void run() {
+            while (playingSquash) {
+                updateCourt();
+                drawCourt();
+                controlFPS();
+            }//ends while
+        }//ends run
+
+        public void updateCourt() {
+            if (racketIsMovingRight) {
+                racketPosition.x = racketPosition.x + 10;
+            }
+            if (racketIsMovingLeft) {
+                racketPosition.x = racketPosition.x - 10;
+            }
+            //detect collisions
+
+            //hit right of screen
+            if (ballPosition.x + ballWidth > screenWidth) {
+                ballIsMovingLeft = true;
+                ballIsMovingRight = false;
+                soundPool.play(sample1, 1, 1, 0, 0, 1);
+            }
+            //hit left of screen
+            if (ballPosition.x < 0) {
+                ballIsMovingLeft = false;
+                ballIsMovingRight = true;
+                soundPool.play(sample1, 1, 1, 0, 0, 1)
+            }
+
+            //Edge of ball has hit bottom of screen
+            if (ballPosition.y > screenHeight - ballWidth) {
+                lives = lives - 1;
+                if (lives == 0) {
+                    lives = 3;
+                    score = 0;
+                    soundPool.play(sample4, 1, 1, 0, 0, 1);
+                }
+
+                ballPosition.y = 1 + ballWidth; //back to top of screen
+
+                //what horizontal direction should we use
+                //for the next falling ball
+                Random randomNumber = new Random();
+                int startX = randomNumber.nextInt(screenWidth - ballWidth) + 1;
+                ballPosition.x = startX + ballWidth;
+
+                int ballDirection = randomNumber.nextInt(3);
+                switch (ballDirection) {
+                    case 0:
+                        ballIsMovingLeft = true;
+                        ballIsMovingRight = false;
+                        break;
+
+                    case 1:
+                        ballIsMovingRight = true;
+                        ballIsMovingLeft = false;
+                        break;
+
+                    case 2:
+                        ballIsMovingLeft = false;
+                        ballIsMovingRight = false;
+                        break;
+                }
+            }
+        }//ends updateCourt
+
+    }//ends runnable
+
+}
